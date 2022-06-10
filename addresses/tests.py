81ea9f3ce0feae897e8e898371afaaea76692203
Welb César <cesarwelb@gmail.com>
2022-06-10 14:30:27 -0300

    Teste unitário para a criação de endereços.

--- addresses/tests.py
+++ addresses/tests.py
@@ -1,3 +1,28 @@
 from django.test import TestCase
 
-# Create your tests here.
+from .models import Address
+
+
+class AddressTestCase(TestCase):
+    def setUp(self):
+        Address.objects.create(
+            postal_code='12345678',
+            address='Rua Rua',
+            number='1',
+            neighbourhood='Centro',
+            state='MG',
+            complement='',
+            description='',
+            city='Varginha'
+        )
+
+    def test_address_creation(self):
+        address = Address.objects.get(address='Rua Rua')
+        self.assertEqual(address.postal_code, '12345678')
+        self.assertEqual(address.address, 'Rua Rua')
+        self.assertEqual(address.number, '1')
+        self.assertEqual(address.neighbourhood, 'Centro')
+        self.assertEqual(address.state, 'MG')
+        self.assertEqual(address.complement, '')
+        self.assertEqual(address.description, '')
+        self.assertEqual(address.city, 'Varginha')
