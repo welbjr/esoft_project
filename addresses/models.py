a4a7d3bfe5f10623e056bc4e336459792b5511c1
Welb César <cesarwelb@gmail.com>
2022-06-10 14:36:23 -0300

    Limpeza: 'null=False' e 'blank=False' já são padrões nos models, então foram removidas.

--- addresses/models.py
+++ addresses/models.py
@@ -3,11 +3,11 @@ from utils.states_choices import STATES
 
 
 class Address(models.Model):
-    postal_code = models.CharField('CEP', max_length=8, null=False, blank=False)
-    address = models.CharField('Endereço', max_length=64, null=False, blank=False)
-    number = models.CharField('Número', max_length=4, null=False, blank=False)
-    neighbourhood = models.CharField('Bairro', max_length=64, null=False, blank=False)
-    state = models.CharField('Estado', max_length=64, choices=STATES, null=False, blank=False)
+    postal_code = models.CharField('CEP', max_length=8)
+    address = models.CharField('Endereço', max_length=64)
+    number = models.CharField('Número', max_length=4)
+    neighbourhood = models.CharField('Bairro', max_length=64)
+    state = models.CharField('Estado', max_length=64, choices=STATES)
     complement = models.CharField('Complemento', max_length=64, null=True, blank=True)
     description = models.CharField('Descrição', max_length=64, null=True, blank=True)
-    city = models.CharField('Cidade', max_length=64, null=False, blank=False)
+    city = models.CharField('Cidade', max_length=64)
