e0f2f1ab6f1946e0e5f583696ee4ea8bd74228a2
Welb César <cesarwelb@gmail.com>
2022-06-12 10:39:16 -0300

    Script que busca o CEP agora roda automaticamente após o usuário entrar os 8 dígitos. Início das melhorias na estética do html, e 'static/css/styles.css' adicionado, com apenas uma classe para esconder um 'alert' de erro.

--- static/js/completeForm.js
+++ static/js/completeForm.js
@@ -1,3 +1,9 @@
+/**
+ * Scrip para buscar o CEP e preencher os campos de endereço automaticamente,
+ * ou mostrar um alerta de erro caso o CEP não seja encontrado.
+ */
+
+// Selecionando os elementos
 const postalCodeInput = document.getElementById("id_postal_code");
 const addressInput = document.getElementById("id_address");
 const numberInput = document.getElementById("id_number");
@@ -6,9 +12,14 @@ const stateInput = document.getElementById("id_state");
 const complementInput = document.getElementById("id_complement");
 const descriptionInput = document.getElementById("id_description");
 const cityInput = document.getElementById("id_city");
+const cepAlert = document.getElementById("cep-alert");
 
 async function completeForm() {
+  /* Essa função busca pelo CEP usando a api 'viacep', e preenche os campos de endereço,
+  ou mostra um alerta de erro caso o CEP não seja encontrado. */
+
   const cep = postalCodeInput.value;
+  cepAlert.classList.add("hidden");
 
   try {
     const response = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
@@ -19,7 +30,14 @@ async function completeForm() {
     neighbourhoodInput.value = bairro;
     cityInput.value = localidade;
   } catch (error) {
-    // todo: mostrar uma notificação de erro para o usuário
-    console.log(error);
+    cepAlert.classList.remove("hidden");
+    addressInput.value = "";
   }
 }
+
+// Faz a busca do CEP quando o usuário digita 8 caracteres
+postalCodeInput.addEventListener("input", () => {
+  if (postalCodeInput.value.length === 8) {
+    completeForm();
+  }
+});
