7b6b8a10da842a3fc03dfadb33414e42706e826d
Welb César <cesarwelb@gmail.com>
2022-06-12 15:28:58 -0300

    'STATICFILES_DIRS' removido.

--- esoft_project/settings.py
+++ esoft_project/settings.py
@@ -126,12 +126,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-]
-
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-# STATIC_ROOT = BASE_DIR / 'static'
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
